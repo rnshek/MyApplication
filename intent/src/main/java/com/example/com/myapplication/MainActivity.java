ab2ef70b7d16b7b54eed5320fe48bd4631bf26e7
RHEE <rjhoon92@naver.com>
2015-12-14 11:46:10 +0900

    hh

--- intent/src/main/java/com/example/com/myapplication/MainActivity.java
+++ intent/src/main/java/com/example/com/myapplication/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.com.intent.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
